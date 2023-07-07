fb7fa64c6107bf79d2a8bbf5c2fcd9fa97f2f843
Asphalt10 <himanshi11panwar@gmail.com>
2023-07-07 12:44:00 +0530

    zig-zag pattern

---
+++ ZigZagPattern.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    public static String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        int n = s.length();
+        int diff = 2 * (numRows - 1);
+        int diagonalDiff = diff;
+        int secondIndex;
+        int index;
+        for (int i = 0; i < numRows; i++) {
+            index = i;
+
+            while (index < n) {
+                answer.append(s.charAt(index));
+                if (i != 0 && i != numRows - 1) {
+                    diagonalDiff = diff-2*i;
+                    secondIndex = index + diagonalDiff;
+                    
+                    if (secondIndex < n) {
+                        answer.append(s.charAt(secondIndex));
+                    }
+                }
+                index += diff;
+            }
+        }
+        
+        return answer.toString();
+    }
+
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+
+        String s=sc.next();
+        int numRows=sc.nextInt();
+
+        sc.close();
+        String result=convert(s,numRows);
+        System.out.println(result);
+    }
+}
